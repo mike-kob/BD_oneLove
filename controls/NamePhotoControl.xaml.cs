86a52b18a621d020d0a4eb7672dacb49045389b8
dashayaskova <dashayaskova111@gmail.com>
2019-04-27 14:15:13 +0300

    Making control for photo and name

---
+++ controls/NamePhotoControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace BD_oneLove.controls
+{
+    /// <summary>
+    /// Логика взаимодействия для NamePhotoControl.xaml
+    /// </summary>
+        public partial class NamePhotoControl : UserControl
+        {
+
+  
+
+
+        public NamePhotoControl()
+            {
+                InitializeComponent();
+            }
+
+            public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register
+           (
+          "Caption",
+          typeof(string),
+          typeof(NamePhotoControl),
+          new PropertyMetadata("")
+           );
+
+            public string Caption
+            {
+                get { return (string)GetValue(CaptionProperty); }
+                set { SetValue(CaptionProperty, value); }
+            }
+        }
+}
