484b9c12d42d7a58c44367118235942f757ffe70
mike-kob <37714867+mike-kob@users.noreply.github.com>
2019-05-06 22:43:32 +0300

    Finish SocialPassportView

---
+++ Views/UsersViews/SocialPassportView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using BD_oneLove.Tools.Navigation;
+using BD_oneLove.ViewModels.UsersViewModels;
+
+namespace BD_oneLove.Views.UsersViews
+{
+    /// <summary>
+    /// Interaction logic for SocialPassportView.xaml
+    /// </summary>
+    public partial class SocialPassportView : UserControl, INavigatable
+    {
+        public SocialPassportView()
+        {
+            InitializeComponent();
+            DataContext = new SocialPassportViewModel();
+        }
+    }
+}
